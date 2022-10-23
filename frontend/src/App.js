b68547d2e851c4609abf55902671eeebc59139dd
Daniel George <degtrdg@github.com>
2022-10-22 21:33:15 -0500

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,22 @@ function App() {
   const [value, setValue] = useState("");
   const [output, setOutput] = useState("");
   const [errors, setErrors] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
     if (value !== "") {
+      setLoading(true);
       axios
         .post("https://chem-autocomplete.herokuapp.com/predict", {
           sentence: value,
         })
         .then((response) => {
+          setLoading(false);
           setOutput(response.data["image"]);
         })
         .catch((err) => {
           console.log(err);
+          setLoading(false);
           setValue("");
           setOutput("");
           setErrors(true);
@@ -102,6 +106,20 @@ function App() {
             ) : (
               <img src={"data:image/png;base64," + output}></img>
             )}
+            {/* Loading spinner*/}
+            {loading ? (
+              <div class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+                <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+                <h2 class="text-center text-white text-xl font-semibold">
+                  Loading...
+                </h2>
+                <p class="w-1/3 text-center text-white">
+                  This may take a few seconds, please don't close this page.
+                </p>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         </div>
         <div class="bg-white">
@@ -116,6 +134,10 @@ function App() {
             </div>
             <div>This is an example of how it can be used:</div>
             <img src={require("./ex_prediction.png")} class="w-1/2"></img>
+            <div>
+              Note: This is hosted on a free Heroku server, so it may take a
+              while to load the first time.
+            </div>
           </div>
         </div>
       </div>
